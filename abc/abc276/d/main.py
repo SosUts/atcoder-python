8040b85d1e6238eca40d2dda38bffa946a7d06fe
SosUts <uts.199.ubosou@gmail.com>
2022-12-09 16:29:15 +0000

    abc276のdまで

--- abc/abc276/d/main.py
+++ abc/abc276/d/main.py
@@ -1,3 +1,31 @@
 import sys
-read = sys.stdin.read
-N, *sample = read().split()
+import math
+math.gcd(2, 4)
+N = int(input())
+A = [x for x in map(int, input().split())]
+
+if len(set(A)) == 1:
+    print(0)
+    sys.exit()
+ans = 0
+g = 0
+for a in A:
+    g = math.gcd(a, g)
+l = []
+
+"""
+gcd(A)でやめないと最小回数にならない
+(例)
+3
+15 30 45なら15でやめないといけない
+"""
+for a in A:
+    a /= g
+    while a%2 == 0:
+        a //= 2
+        ans += 1
+    while a%3 == 0:
+        a //= 3
+        ans += 1
+    l.append(a)
+print(ans if len(set(l)) == 1 else -1)
